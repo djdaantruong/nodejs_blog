02d657d44d8cde2716a86d101bb0b5f466941aef
djdaantruong <daantd.a12017@gmail.com>
2020-08-30 10:44:02 +0700

    Read from database

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,17 +1,15 @@
 const Course = require('../models/Course');
+const { mutipleMongooseToObject } = require('../../util/mongoose');
 class SiteController {
     // GET / home
-    index(req, res) {
+    index(req, res, next) {
 
-        Course.find({}, function (err, courses) {
-            if(!err){
-                res.json(courses);
-            } else{
-                res.status(400).json({ error: 'ERROR!!!'});
-            }
-        });
+        Course.find({})
+            .then(courses => res.render('home', { 
+                courses: mutipleMongooseToObject(courses)
+            }))
+            .catch(next);
 
-        // res.render('home');
     }
 
     // GET / search
