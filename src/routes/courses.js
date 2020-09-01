c5506d6171da45718c41196fcf51fb06400ac927
djdaantruong <daantd.a12017@gmail.com>
2020-09-01 23:10:15 +0700

    Update course

--- src/routes/courses.js
+++ src/routes/courses.js
@@ -6,6 +6,8 @@ const courseController = require('../app/controllers/CourseController');
 //tính chất của router là met từ trên xuống dưới và chỉ met một cái mà thôi
 router.get('/create', courseController.create);
 router.post('/store', courseController.store);
+router.get('/:id/edit', courseController.edit);
+router.put('/:id', courseController.update);
 router.get('/:slug', courseController.show);
 
 module.exports = router;
