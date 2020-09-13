7de53d7a4c054195c775bb6c9d22183df55e97e8
djdaantruong <daantd.a12017@gmail.com>
2020-09-14 00:28:29 +0700

    Soft delete

--- src/app/controllers/CourseController.js
+++ src/app/controllers/CourseController.js
@@ -19,11 +19,10 @@ class CourseController {
 
     // [POST] /course/store
     store(req, res, next) {
-        const formData = req.body;
-        formData.image = `https://img.youtube.com/vi/${req.body.videoId}/sddefault.jpg`;
-        const course = new Course(formData);
+        req.body.image = `https://img.youtube.com/vi/${req.body.videoId}/sddefault.jpg`;
+        const course = new Course(req.body);
         course.save()
-            .then(() => res.redirect('/'))
+            .then(() => res.redirect('/me/stored/courses'))
             .catch(error => {
 
             });
@@ -43,15 +42,27 @@ class CourseController {
         Course.updateOne({_id: req.params.id}, req.body)
             .then(() => res.redirect('/me/stored/courses'))
             .catch(next);
-        
     }
 
     // [DELETE] /course/:id
     destroy(req, res, next) {
+        Course.delete({_id: req.params.id})
+            .then(() => res.redirect('back'))
+            .catch(next);
+    }
+
+    // [DELETE] /course/:id/force
+    forceDestroy(req, res, next) {
         Course.deleteOne({_id: req.params.id})
             .then(() => res.redirect('back'))
             .catch(next);
-        
+    }
+
+    // [PATCH] /course/:id/retstore
+    restore(req, res, next) {
+        Course.restore({_id: req.params.id})
+            .then(() => res.redirect('back'))
+            .catch(next);
     }
 
 }
