d137fa16f92b29a77e99fa5dc180d16256b5fa11
djdaantruong <daantd.a12017@gmail.com>
2020-10-12 15:54:10 +0700

    Autoincrement _id field

--- src/app/models/Course.js
+++ src/app/models/Course.js
@@ -1,10 +1,12 @@
 const mongoose = require('mongoose');
 const slug = require('mongoose-slug-generator');
 const mongooseDelete = require('mongoose-delete');
+const AutoIncrement = require('mongoose-sequence')(mongoose);
 
 const Schema = mongoose.Schema;
 
 const Course = new Schema({
+    _id: {type: Number},
     name: { type: String, require: true},
     description: { type: String },
     image: { type: String },
@@ -12,11 +14,13 @@ const Course = new Schema({
     level: { type: String },
     slug: { type: String, slug: 'name', unique: true },
 }, {
+    _id: false,
     timestamps: true,
 });
 
 //Add mongoose pluggins
 mongoose.plugin(slug);
+Course.plugin(AutoIncrement);
 Course.plugin(mongooseDelete, { 
     deletedAt : true,
     overrideMethods: 'all' 
